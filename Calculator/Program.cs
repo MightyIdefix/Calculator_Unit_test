e3ee8969001170372fdd0788b7c0a3af4e07d632
MightyIdefix <nicolai@nicolaifisker.dk>
2021-03-03 14:00:53 +0100

    first commit

---
+++ Calculator/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class Calc
+    {
+        public double Add(double a, double b)
+        {
+            Accumulator = a + b;
+            return Accumulator;
+        }
+
+        public double Sub(double a, double b)
+        {
+            Accumulator = a - b;
+            return Accumulator;
+        }
+
+        public double Multi(double a, double b)
+        {
+            Accumulator = a * b;
+            return Accumulator;
+        }
+
+        public double Power(double x, double exp)
+        {
+            Accumulator = Math.Pow(x, exp);
+            return Accumulator;
+        }
+
+        public double Divide(double dividend, double divisor)
+        {
+            if (divisor == 0) throw new DivisorException(divisor);
+            Accumulator = dividend / divisor;
+            return Accumulator;
+        }
+
+        public double Accumulator { get; private set; }
+
+        public void Clear()
+        {
+            Accumulator = 0.0;
+        }
+
+        public double Sqrt(double x)
+        {
+            Accumulator = Math.Sqrt(x);
+            return Accumulator;
+        }
+
+        public double Add(double addend)
+        {
+            Accumulator += addend;
+            return Accumulator;
+        }
+
+        public double Subtract(double subtractor)
+        {
+            Accumulator -= subtractor;
+            return Accumulator;
+        }
+
+        public double Multiply(double multiplier)
+        {
+            Accumulator *= multiplier;
+            return Accumulator;
+        }
+
+        public double Divide(double divisor)
+        {
+            if (divisor == 0) throw new DivisorException(divisor);
+            Accumulator /= divisor;
+            return Accumulator;
+        }
+
+        public double Power(double exponent)
+        {
+            Accumulator = Math.Pow(Accumulator, exponent);
+            return Accumulator;
+        }
+
+    }
+}
+
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Calc test = new Calc();
+            Console.WriteLine(test.Add(2, 2));
+            Console.WriteLine(test.Sub(4, 2));
+            Console.WriteLine(test.Multi(3, 3));
+            Console.WriteLine(test.Power(3, 3));
+            Console.WriteLine(test.Sqrt(9));
+
+        }
+    }
+}
---
+++ Calculator_Test/Class1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Calculator
+{
+    [TestFixture]
+    public class Class1
+    {
+        private Calc uut;
+        [SetUp]
+
+        public void Setup()
+        {
+            uut = new Calc();
+        }
+
+        [TestCase(2, 2, 4)]
+        [TestCase(10, 10, 20)]
+        [TestCase(27, 13, 40)]
+        [TestCase(-10, -5, -15)]
+        public void addCalculator_Test(double a, double b, double c)
+        {
+            //Arrange
+            //Act
+            uut.Add(a, b);
+            //Assert
+            Assert.That(uut.Add(a, b), Is.EqualTo(c));
+        }
+
+
+        [TestCase(4, 2, 2)]
+        [TestCase(8, 3, 5)]
+        [TestCase(10, 10, 0)]
+        [TestCase(50, 60, -10)]
+        public void SubCalculator_Test(double a, double b, double c)
+        {
+            //Arrange
+            //Act
+            uut.Sub(a, b);
+            //Assert
+            Assert.That(uut.Sub(a, b), Is.EqualTo(c));
+        }
+
+        [TestCase(4, 4, 16)]
+        [TestCase(2, 5, 10)]
+        [TestCase(10, 10, 100)]
+        [TestCase(-7, -3, 21)]
+        public void MultiCalculator_Test(double a, double b, double c)
+        {
+            //Arrange
+            //Act
+            uut.Multi(a, b);
+            //Assert
+            Assert.That(uut.Multi(a, b), Is.EqualTo(c));
+        }
+
+        [TestCase(3, 3, 27)]
+        [TestCase(-3, -3, -0.037037037037037035)]
+        [TestCase(0, 0, 1)]
+        [TestCase(-4, 2, 16)]
+        public void PowerCalculator_Test(double a, double b, double c)
+        {
+            //Arrange
+            //Act
+            uut.Power(a, b);
+            //Assert
+            Assert.That(uut.Power(a, b), Is.EqualTo(c));
+        }
+
+        [TestCase(9, 3)]
+        [TestCase(4, 2)]
+        [TestCase(16, 4)]
+        [TestCase(42, 6.4807406984078604)]
+        public void SqrtCalculator_Test(double a, double b)
+        {
+            //Arrange
+            //Act
+            uut.Sqrt(a);
+            //Assert
+            Assert.That(uut.Sqrt(a), Is.EqualTo(b));
+        }
+
+        [TestCase(8, 4, 2)]
+        [TestCase(10, 2, 5)]
+        [TestCase(9, 9, 1)]
+        [TestCase(6, 4, 1.5)]
+        public void DivideCalculator_Test(double a, double b, double c)
+        {
+            //Arrange
+            //Act
+            uut.Divide(a, b);
+            //Assert
+            Assert.That(uut.Divide(a, b), Is.EqualTo(c));
+        }
+
+        [TestCase(2, 2, 4)]
+        [TestCase(4, 6, 10)]
+        [TestCase(3, 5, 8)]
+        [TestCase(-1, -5, -6)]
+        public void Accumulator_Test(double a, double b, double c)
+        {
+            //Arrange
+            //Act
+            uut.Add(a, b);
+            //Assert
+            Assert.That(uut.Accumulator, Is.EqualTo(c));
+        }
+
+        [TestCase(2, 2, 0)]
+        [TestCase(4, 6, 0)]
+        [TestCase(3, 5, 0)]
+        [TestCase(-1, -5, 0)]
+        public void AccumulatorClear_Test(double a, double b, double c)
+        {
+            //Arrange
+            //Act
+            uut.Add(a, b);
+            uut.Clear();
+            //Assert
+            Assert.That(uut.Accumulator, Is.EqualTo(c));
+        }
+
+        [TestCase(-76, 0)]
+        [TestCase(62, 0)]
+        [TestCase(7, 0)]
+        public void TestThatExceptionThrownWhenDividingByZero(double a, double b)
+        {
+            //Arrange
+            //Act
+            //Assert
+            Assert.Throws<DivisorException>(() => uut.Divide(a, b));
+        }
+
+        [TestCase(-7, 0)]
+        [TestCase(2, 0)]
+        [TestCase(1, 0)]
+        public void TestThatExceptionThrownWhenDividingByZeroUsingAccumulator(double a, double b)
+        {
+            //Arrange
+            //Act
+            uut.Add(a, b);
+            //Assert
+            Assert.Throws<DivisorException>(() => uut.Divide(b));
+        }
+    }
+}
+
+
+
+/*
+ * +
+ * -
+ * *
+ * "/
+ * ^
+ */
